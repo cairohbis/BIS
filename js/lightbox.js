20f69700e20f25cfdc5b3327520800d55988e0db
cairohbis <mrn57907@gmail.com>
2026-08-12 23:19:29 +0300

    Delete js/lightbox.js

--- js/lightbox.js
+++
@@ -1,8 +0,0 @@
-/* ══════════════════════════════════════════
-   js/lightbox.js — عرض الصور بالتكبير (Lightbox)
-══════════════════════════════════════════ */
-window.openLightbox = function(src) {
-  document.getElementById("lightboxImg").src = src;
-  document.getElementById("lightbox").classList.add("show");
-};
-
